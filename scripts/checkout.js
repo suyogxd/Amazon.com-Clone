dd7f891190b840020c755d336179ead3f84bd146
suyogxd <108789515+suyogxd@users.noreply.github.com>
2024-08-28 19:01:19 +0545

    Add files via upload

---
+++ scripts/checkout.js
@@ -0,0 +1,20 @@
+import { renderOrderSummary } from  './checkout/orderSummary.js';
+import { renderPaymentSummary } from './checkout/paymentSummary.js';
+//import '../data/cart-class.js'
+// import '../data/backend-practice.js'
+import { loadProducts } from '../data/products.js';
+
+
+new Promise ((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+}).then(() =>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+// loadProducts(() => {
+    
+// });
+
